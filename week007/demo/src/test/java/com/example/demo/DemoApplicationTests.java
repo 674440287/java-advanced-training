44639239b015033125315771f16cb5670427cea5
lyp <lyp@qq.com>
2021-05-07 14:18:23 +0800

    a

--- week007/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ week007/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+    @Autowired
+    @Qualifier("dataSource1")
+    DataSource dataSource;
+
+    @Autowired
+    @Qualifier("dataSource2")
+    DataSource dataSource2;
+
     @Test
-    void contextLoads() {
+    void contextLoads(){
+        System.out.println("--------------");
+        System.out.println(dataSource);
+        System.out.println("--------------");
+        System.out.println(dataSource2);
     }
 
 }
